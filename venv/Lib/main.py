3133157726336c0e17b4af862624826be315edfa
Genevieve Will <64991919+genefwill@users.noreply.github.com>
2021-07-09 22:07:10 -0700

    Trying to get this to work.

--- venv/Lib/main.py
+++ venv/Lib/main.py
@@ -7,10 +7,12 @@ pygame.init()
 
 pygame.display.set_caption('Pygame Window')
 
+#Window and display size
 WINDOW_SIZE = (600,400)
 screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
 display = pygame.Surface((600,400))
 
+#Images for game
 player_image = pygame.image.load('cat.png')
 floor_image = pygame.image.load('check.png')
 FLOOR_TILE = floor_image.get_width()
@@ -21,9 +23,17 @@ WHITE_TILE = floor_image.get_width()
 black_image = pygame.image.load('black.png')
 BLACK_TILE = shelf_image.get_width()
 
+#creating scroll variable
 scroll = [0,0]
 
+
+
 def load_map(path):
+    """
+    function to load text file game map
+    :param path: game map as text file
+    :return: game map read out
+    """
     f = open(path + '.txt','r')
     data = f.read()
     f.close()
@@ -34,6 +44,8 @@ def load_map(path):
     return game_map
 game_map = load_map('map')
 
+
+#old game map that I didn't want to erase because I spent a lot of time writing it
 game_map2 = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
@@ -48,7 +60,12 @@ game_map2 = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0
             ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
             ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
 
+
+
 def collision_test(rect, tiles):
+    """
+    Function that determines when player collides with a tile
+    """
     hit_list = []
     for tile in tiles:
         if rect.colliderect(tile):
@@ -56,6 +73,13 @@ def collision_test(rect, tiles):
     return hit_list
 
 def move(rect,movement,tiles):
+    """
+    Function to define movement
+    :param rect:
+    :param movement:
+    :param tiles:
+    :return:
+    """
     collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
     rect.x += movement[0]
     hit_list = collision_test(rect, tiles)
@@ -77,24 +101,32 @@ def move(rect,movement,tiles):
             collision_types['top'] = True
     return rect, collision_types
 
+#Establishing moving right and left variable
 moving_right = False
 moving_left = False
 
+#Defining player location, momentum and air time variable
 player_location = [50,300]
 player_y_momentum = 0
 air_timer = 0
 
+#Defining player rectangle
 player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
-test_rect = pygame.Rect(100, 100, 100,50)
+
+#Game loop
 while True:
+    #fills display with color
     display.fill((146,244,255))
 
+    #defining scroll value based on character location
     scroll[0] += (player_rect.x - scroll[0]- 315)
     scroll[1] += (player_rect.y - scroll[1] - 300)
 
+    #defining list of tile rectangles for map
     tile_rects = []
     y = 0
     for row in game_map:
+        #filling out game map
         x = 0
         for tile in row:
             if tile == '1':
@@ -115,8 +147,9 @@ while True:
 
 
 
-
+    #defining character movement variable
     player_movement = [0,0]
+    #Establing how player moves based on moving variable
     if moving_right:
         player_movement[0] += 2
     if moving_left:
@@ -128,7 +161,7 @@ while True:
 
 
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
-
+    #Checking if character collides with bottom or top of platform and establishes a jump time
     if collisions['bottom']:
         player_y_momentum = 0
         air_timer = 0
@@ -138,13 +171,10 @@ while True:
     if collisions['top']:
         player_y_momentum = 0
 
+    #displaying of player
     display.blit(player_image, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
-    if player_rect.colliderect(test_rect):
-        pygame.draw.rect(screen,(255,0,0),test_rect)
-    else:
-        pygame.draw.rect(screen, (0, 0, 0), test_rect)
-
+    #Establishes movement by keystroke and Quiting of game loop
     for event in pygame.event.get():
         if event.type == QUIT:
 
@@ -164,7 +194,10 @@ while True:
             if event.key == K_LEFT:
                 moving_left = False
 
+    #scaling of game window
     surf = pygame.transform.scale(display, WINDOW_SIZE)
     screen.blit(surf,(0,0))
+    #updating display
     pygame.display.update()
+    #game clock
     clock.tick(60)
